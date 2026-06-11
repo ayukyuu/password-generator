0c13e1c41e58e7690a8526a0e35e1c605e0b998f
ayukyuu <arya72480@gmail.com>
2026-06-11 14:28:25 +0200

    complete basic generator script

---
+++ script.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("generator-button");
+const firstPassElement = document.getElementById("first-password");
+const secondPassElement = document.getElementById("second-password");
+const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?`~".split(
+        "",
+    );
+let length = 16;
+let firstPassword = "";
+let secondPassword = "";
+
+button.addEventListener("click", () => {
+    firstPassword = generatePassword();
+    secondPassword = generatePassword();
+    firstPassElement.textContent = firstPassword;
+    secondPassElement.textContent = secondPassword;
+});
+
+function generatePassword() {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return password;
+}
